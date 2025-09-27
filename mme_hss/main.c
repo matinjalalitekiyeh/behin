191e22c2f7d1d5def67161f68a2b82d5c9a56e4a
matin <matin@siz-tel.com>
2025-09-27 03:32:26 -0400

    create context

--- mme_hss/main.c
+++ mme_hss/main.c
@@ -10,39 +10,46 @@
 
 
 int main() {
-    int sock_raw;
+    sock_context_t *sock = (sock_context_t*)malloc(sizeof(sock_context_t));
+
+    sock->buffer = (u8*)malloc(BUFFER_SIZE * sizeof(char));
+    pthread_mutex_init(&sock->lock, NULL);
+
+//    int sock_raw;
     struct sockaddr saddr;
     socklen_t saddr_size = sizeof(saddr);
-    unsigned char buffer[BUFFER_SIZE];
+//    unsigned char buffer[BUFFER_SIZE];
 
     pcap_init(OUTPUT_FILE);
 
-    sock_raw = socket_create_raw();
-    if (sock_raw < 0) {
+    sock->sockfd = socket_create_raw();
+    if (sock->sockfd < 0) {
         pcap_close();
         exit(EXIT_FAILURE);
     }
 
-    socket_set_timeout(sock_raw, 1);
+
+
+    socket_set_timeout(sock->sockfd, 1);
 
 
     while (1) {
-        int data_size = socket_receive(sock_raw, buffer, BUFFER_SIZE, &saddr, &saddr_size);
-        if (data_size < 0) {
+        sock->buffer_size = socket_receive(sock->sockfd, sock->buffer, BUFFER_SIZE, &saddr, &saddr_size);
+        if (sock->buffer_size < 0) {
             if (errno == EAGAIN || errno == EWOULDBLOCK) continue;
             perror("Recvfrom error");
             break;
         }
 
         bool is_retrans = false;
-        if (is_diam_packet(buffer, data_size, &is_retrans)) {
+        if (is_diam_packet(sock->buffer, sock->buffer_size, &is_retrans)) {
             if (!is_retrans) {
-                pcap_write_packet(buffer, (size_t)data_size);
+                pcap_write_packet(sock->buffer, (size_t)sock->buffer_size);
             }
         }
     }
 
-    socket_close(sock_raw);
+    socket_close(sock->sockfd);
     pcap_close();
     return 0;
 }
--- src/socket/socket_mdl.h
+++ src/socket/socket_mdl.h
@@ -5,7 +5,36 @@
 #include <netinet/ip.h>
 #include <linux/if_ether.h>
 
+#include <pthread.h>
+
 #define BUFFER_SIZE 65536
+
+#define SOCKET_MDL_ERROR_MSG_LEN 256
+
+typedef uint8_t   u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef enum {
+    SOCKET_SUCCESS = 0,
+    SOCKET_ERROR_INVALID_PARAM = -1,
+    SOCKET_ERROR_MEMORY = -2,
+    SOCKET_ERROR_IO = -3,
+    SOCKET_ERROR_TIMEOUT = -4,
+    SOCKET_ERROR_NOT_READY = -5,
+} sock_res_t;
+
+typedef struct {//__attribute__((packed))
+    pthread_mutex_t lock;
+    int sockfd;
+    u32 timeout_sec;
+
+    u8 *buffer;
+    ssize_t buffer_size;
+    char last_error[SOCKET_MDL_ERROR_MSG_LEN];
+} sock_context_t;
+
 
 // Socket functions
 int socket_create_raw();
